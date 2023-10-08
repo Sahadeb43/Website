1182137ecb5efa91558f4475c66267ee41edd088
Sahadeb43 <147254552+Sahadeb43@users.noreply.github.com>
2023-10-08 17:24:13 +0600

    Add files via upload

---
+++ Jv/script.js
@@ -0,0 +1,11 @@
+<script>
+  function openIframe(url) {
+    var iframe = document.createElement('iframe');
+    iframe.src = url;
+    iframe.width = '100%';
+    iframe.height = '100%';
+    iframe.style.border = 'none';
+    document.body.appendChild(iframe);
+  }
+</script>
+
